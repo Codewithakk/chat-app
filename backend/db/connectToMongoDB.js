c89a027b4208d5d4d166823f54cdabe44a203d6a
Akshay Kumar Singh <140572866+Codewithakk@users.noreply.github.com>
2024-08-06 13:19:32 +0530

    changes done

---
+++ backend/db/connectToMongoDB.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const connectToMongoDB = async () => {
+	try {
+		await mongoose.connect(`mongodb+srv://Akshaysingh:eJJC8MxIija0NFPG@projectsofakshay.zlw2xbe.mongodb.net/chatapptest?retryWrites=true&w=majority`);
+		console.log("Connected to MongoDB");
+	} catch (error) {
+		console.log("Error connecting to MongoDB", error.message);
+	}
+};
+
+export default connectToMongoDB;
